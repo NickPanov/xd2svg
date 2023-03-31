9418bd6a186111ae48cd3dc4c2f651ba41820697
NickPanov <nickpanov@outlook.com>
2023-03-31 17:51:50 +0300

    2+ keyframes support refactored

--- main.js
+++ main.js
@@ -1,32 +1,14 @@
 const { selection } = require("scenegraph");
 const fs = require("uxp").storage.localFileSystem;
-const { Timeline } = require("./xd2svg"); 
-   
+const { Timeline } = require("./xd2svg");
+
 let clipboard = require("clipboard");
-let panel;
-let debug; 
+let panel; 
 let keyframes;
 let submitbutton;
 let warning;
-
-
-class Debug {
-    body;
-    constructor(element) {
-        this.body = element;
-        this.body.className = 'debug';
-        panel.appendChild(this.body);
-    }
-    clear() {
-        this.body.innerHTML = '';
-    }
-    add(content) {
-        let newparagraph = document.createElement('p');
-        newparagraph.className = 'debug-paragraph';
-        newparagraph.innerHTML = content;
-        this.body.appendChild(newparagraph);
-    }
-}
+let animationOptions = {};
+ 
 class Keyframes {
     body;
     constructor(element) {
@@ -43,124 +25,19 @@ class Keyframes {
     }
 }
  
-function flattenArtboards(artboards) {
-    let result = [];
-    artboards.forEach(artboard => {
-        let flattened = [];
-        const traverse = (node) => {
-            flattened.push(node);
-            if (node.children) {
-                node.children.forEach((child) => traverse(child));
-            }
-        };
-        artboard.children.forEach((obj) => traverse(obj));
-        result.push({
-            keyframe: artboard,
-            keyframeNodes: flattened
-        })
-    })
-    return result;
-}
 
-function getCSS(keyframes) {
 
-    let cssRuleset = {
-        from: [],
-        to: []
-    };
 
-    function createCSSItem(node) {
-
-        return {
-            id: node.name,
-            path: node.pathData || false,
-            translateX: node.translation.x,
-            translateY: node.translation.y,
-            rotation: node.rotation,
-            fill: node.fillEnabled ? node.fill.toRgba() : false,
-            stroke: node.stroke.toRgba()
-        }
-    }
-    keyframes.forEach((keyframe, keyframeIndex) => {
-
-        keyframe.keyframeNodes.forEach(node => {
-            let prevKeyframe = keyframes[keyframeIndex - 1];
-
-            if (keyframeIndex == 0) {
-                cssRuleset.from.push(createCSSItem(node))
-            }
-            else {
-                if (prevKeyframe.keyframeNodes.find(pkfn => pkfn.name == node.name)) {
-                    cssRuleset.to.push(createCSSItem(node))
-                }
-            }
-        });
-
-    })
-    let cssString = ``;
-    cssRuleset.to.forEach(to => {
- 
-
-        let from = cssRuleset.from.find(from => from.id == to.id);
-        cssString += `\u0040keyframes animation-${to.id}{
-                from {
-                    d:path("${from.path}");
-                    stroke:${from.stroke};`
-        cssString += from.fill ? `fill:${from.fill};` : ``;
-        cssString += `transform:translate(${from.translateX}px, ${from.translateY}px);
-                }
-                to {
-                    d:path("${to.path}");
-                    stroke:${to.stroke};`
-        cssString += to.fill ? `fill:${to.fill};` : ``;
-        cssString += `transform:translate(${to.translateX}px, ${to.translateY}px);
-
-                }
-            }
-            #${to.id}{
-                animation: 2s infinite alternate animation-${to.id};
-            }
-            `;
-
-    })
-
-    return cssString;
-}
-function SVGTemplate(firstFrame, cssCode) {
-
-    let svgstring = ``;
-
-    svgstring += `<svg width="${firstFrame.keyframe.width}" height="${firstFrame.keyframe.height}" viewBox="0 0 ${firstFrame.keyframe.width} ${firstFrame.keyframe.height}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">`
-    svgstring += `
-    <defs>
-        <style>
-        ${cssCode}
-        </style>
-    </defs>`;
-
-    firstFrame.keyframeNodes.forEach(node => {
-        svgstring += `<path id="${node.name}"`
-        if (node.pathData) svgstring += ` d="${node.pathData}"`
-        if (node.fillEnabled) svgstring += ` fill="${node.fill.toRgba()}"`
-
-        svgstring += ` transform="translate(${node.translation.x} ${node.translation.y})"`
-        svgstring += `/>`
-    })
-
-    svgstring += `</svg>`;
-    return svgstring;
-}
 async function generateCode() {
 
+    animationOptions.duration = panel.querySelector('#duration').value;
+    animationOptions.interations = panel.querySelector('#iterations').value;
+    animationOptions.direction = panel.querySelector('#direction').value; 
+    animationOptions.easing = panel.querySelector('#easing').value;
     //we have the global selection.items; 
-     let timeline = new Timeline(selection.items);  
+    let timeline = new Timeline(selection.items,animationOptions);
 
-    let keyframes = flattenArtboards(selection.items);
-
-    //keyframes.map(keyframe => new XD2SVG.Keyframe)
-
-    let cssCode = getCSS(keyframes).toString();
-    let SVGContent = SVGTemplate(keyframes[0], cssCode);
+    let SVGContent = timeline.svg();
 
     clipboard.copyText(SVGContent);
     const svgfile = await fs.getFileForSaving("animation.svg");
@@ -174,7 +51,7 @@ async function getHtmlTemplate(template) {
     let html = await entry.read()
     return html
 }
-async function create() {
+async function create() { 
 
     let HTML = await getHtmlTemplate('panel');
     panel = document.createElement("div");
@@ -183,8 +60,10 @@ async function create() {
     warning = panel.querySelector(".alert-warning");
     submitbutton = panel.querySelector("#ok");
     keyframes = new Keyframes(panel.querySelector("#artboard-list"));
-    debug = new Debug(document.createElement("div"));
-    return panel;
+
+   
+       
+    return panel; 
 }
 
 async function show(event) {
@@ -192,9 +71,8 @@ async function show(event) {
 }
 
 function update() {
-    const { Artboard } = require("scenegraph"); 
-     
-    debug.clear();
+    const { Artboard } = require("scenegraph");
+ 
     keyframes.clear();
 
     let artboardCount = selection.items.filter(item => item instanceof Artboard).length;
@@ -203,7 +81,7 @@ function update() {
         warning.classList.add('show')
         submitbutton.disabled = true
     }
-    else { 
+    else {
         warning.classList.remove('show')
         warning.classList.add('hide')
         submitbutton.disabled = false
--- xd2svg.js
+++ xd2svg.js
@@ -1,27 +1,85 @@
 class Timeline {
     Keyframes;
+    SceneNodes;
+    Layers;
     duration;
     iterations;
     easing;
-    constructor(artboards, options) {
+    animationOptions;
+    constructor(artboards, OPTIONS) {
+        let _self = this;
         this.Keyframes = artboards.map(artboard => new Keyframe(artboard));
-        
-    }
-    css(){
+
+        this.SceneNodes = (() => {
+            let arr = [];
+            this.Keyframes.forEach(keyframe => {
+                keyframe.ChildNodeList.forEach(node => {
+                    node.parentKeyframe = this.Keyframes.indexOf(keyframe);
+                    arr.push(node);
+                })
+            })
+            return arr;
+        })();
+        //return Unique nodes as Layers;
+        this.Layers = [...new Set(this.SceneNodes.map(item => item.id))].map(nodeid => new Layer(nodeid, _self))
+        let iterations  = OPTIONS.iterations > 0 ? OPTIONS.iterations : 'infinite';
+        this.animationOptions = `${OPTIONS.duration}s ${OPTIONS.easing} 0s ${iterations} ${OPTIONS.direction};` 
 
     }
-    svg(){
+    css() {
+
+        let cssString = ``;
+
+        this.Layers.forEach(layer => {
+            cssString += layer.css;
+        });
+        this.Layers.forEach(layer => {
+            cssString += `#${layer.id} {animation: keyframes-${layer.id} ${this.animationOptions} }`
+        })
+
+        return cssString;
+    }
+    svg() {
+        let firstFrame = this.Keyframes[0];
+        let cssCode = this.css();
+ 
+        let svgstring = ``;
+      
+
+        svgstring += `<svg width="${firstFrame.width}" height="${firstFrame.height}" viewBox="0 0 ${firstFrame.width} ${firstFrame.height}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">`
+        svgstring += `
+            <defs>
+                <style>
+                ${cssCode}
+                </style>
+            </defs>`;
+
+            console.log("GETTING SVG");
+
+        firstFrame.ChildNodeList.forEach(node => {
+            svgstring += `<path id="${node.id}"`
+            if (node.rawPath) svgstring += ` d="${node.rawPath}"`
+            svgstring += ` fill="${node.fill}"` 
+            svgstring += ` transform="translate(${node.translation.x} ${node.translation.y})"`
+            svgstring += `/>`
+        })
+
+        svgstring += `</svg>`;
+        return svgstring;
 
     }
 
 }
 class Keyframe {
-    NodeList;
+    id;
+    ChildNodeList;
     width;
     height;
     fill;
     constructor(artboard) {
-        this.NodeList = this.flatten(artboard).map(node => new Node(node))
+        this.id = artboard.name.split(' ').join('_');
+
+        this.ChildNodeList = this.flatten(artboard).map(node => new ChildNode(node))
     }
     flatten(artboard) {
         let flattened = [];
@@ -35,17 +93,80 @@ class Keyframe {
         return flattened;
     }
 }
+class Layer {
+    KeyframeNodes;
+    id;
+    css = ``;
+    constructor(layerID, timeline) {
 
-class Node {
-    constructor(node) {
-        this.id = node.name;
-        this.path = node.pathData || false;
-        this.translateX = node.translation.x;
-        this.translateY = node.translation.y;
+        this.id = layerID;
+
+        this.KeyframeNodes = timeline.SceneNodes.filter(scenenode => scenenode.id == layerID);
+        let keyframeCount = (timeline.Keyframes.length);
+        let keyframeStep = 100 / (keyframeCount - 1);
+
+        function cssKeyframes(keyframeContent) { 
+            return `@keyframes keyframes-${layerID}{
+                        ${keyframeContent}
+                    }`
+        }
+        function cssKeyframesStep(index, content) {
+
+            return `${index * keyframeStep}%{
+                ${content}
+            }`
+        }
+        function cssKeyframesRule(property, value) {
+            return `${property}:${value};`
+        }
+
+        var stepset = ``;
+        var lastmatch;
+        for (var i = 0; i < keyframeCount; i++) {
+
+            let match = this.KeyframeNodes.find(node => node.parentKeyframe == i);
+            lastmatch = match;
+            if (match) {
+                var ruleset = ``;
+                for (const [key, value] of Object.entries(match.cssRules)) {
+                    let rule = cssKeyframesRule(key, value);
+                    ruleset += rule;
+                }
+                stepset += cssKeyframesStep(i, ruleset);
+            }
+            else {
+                stepset += cssKeyframesStep(i, ruleset);
+            }
+            
+        }
+        
+        this.css += cssKeyframes(stepset).replace(':undefined', '');
+
+
+    }
+}
+class ChildNode {
+    //represents single node as a child;
+    cssRules;
+    constructor(node /* XD Layer */) {
+        this.id = node.name.split(' ').join('_'); 
+        this.rawPath = node.pathData; 
+        this.translation = node.translation; 
         this.rotation = node.rotation;
         this.fill = node.fillEnabled ? node.fill.toRgba() : false;
-        this.stroke = node.stroke.toRgba();
+        this.stroke = node.strokeEnabled ? node.stroke.toRgba(): false;
+        this.opacity = node.opacity;
+         
+        this.cssRules = {
+            d:`path("${node.pathData}")` || false,
+            transform:`translate(${node.translation.x}px,${node.translation.y}px) rotate(${this.rotation}deg)`,
+            fill:`rgba(${this.fill.r},${this.fill.g},${this.fill.b},${this.fill.a/100})`,
+            stroke:`rgba(${this.stroke.r},${this.stroke.g},${this.stroke.b},${this.stroke.a/100})`,
+            opacity: this.opacity
+          
+        }
     }
+    
 }
 module.exports = {
     Timeline,
